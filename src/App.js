47f2f6bba68d5a33c207e290916632c3004fdeba
Pjanarthana <janarthananoffl@gmail.com>
2025-03-31 18:07:31 +0530

    last commit for this project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Favorites from './components/Favorites'
 import Modal from './components/Modal'
 import Search from './components/Search';
 import Meals from './components/Meals'
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
 
 
 
@@ -12,6 +14,20 @@ export default function App() {
   const { showModal, favorites } = useGlobalContext()
 
   return (
+    <HelmetProvider>
+    <>
+     <Helmet>
+     <title>Discover Delicious Recipes - Your Meal Guide</title>
+          <meta name="description" content="Find and explore a variety of delicious recipes with easy-to-follow instructions. Save your favorites and cook like a pro!" />
+          <meta name="keywords" content="recipes, meal finder, food, cooking, ingredients, quick recipes, healthy meals, best meals, easy cooking" />
+          <meta property="og:title" content="Discover Delicious Recipes - Your Meal Guide" />
+          <meta property="og:description" content="Explore a world of flavors with our recipe collection. Find your next favorite meal today!" />
+          <meta property="og:image" content="URL_TO_YOUR_IMAGE" />
+          <meta property="og:url" content="YOUR_WEBSITE_URL" />
+          <meta name="twitter:title" content="Discover Delicious Recipes - Your Meal Guide" />
+          <meta name="twitter:description" content="Find easy-to-make, healthy, and delicious recipes from around the world." />
+      </Helmet>
+      
     <main>
 
       <Search />
@@ -20,6 +36,8 @@ export default function App() {
       <Meals />
       {showModal && <Modal />}
     </main>
+    </>
+    </HelmetProvider>
   )
 }
 
--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -4,6 +4,7 @@ const Favorites = () => {
   const { favorites, selectMeal, removeFromFavorites } = useGlobalContext()
 
   return <section className="favorites">
+    
     <div className="favorites-content">
       <h5>Favorites</h5>
       <div className="favorites-container">
